8213f1bb23d65e58a97e37de8aeff345d85852c7
zking63 <zoemking63@gmail.com>
2021-02-07 13:40:15 -0500

    2.7.20

--- algopractice.js
+++ algopractice.js
@@ -6,5 +6,45 @@ function Sigma(num){
     }
     return sum;
 }
-var answer = Sigma(3);
+var answer = Sigma(4);
 console.log(answer);
+
+function factorial(num2){
+    mult = 1;
+    for (var j = 0; j < num2 +1; j++)
+    if (j > 0){
+        var mult = mult * j;
+    }
+    return mult;
+}
+var answer2 = factorial(5);
+console.log(answer2);
+
+function generatecoinchange(cents){
+    penny = 1;
+    nickle = 5;
+    dime = 10;
+    quarter = 25;
+    for (var p = 0; p < cents +1; p++){
+        if(cents % quarter == 0){
+            final = cents/quarter
+            return final;
+        }
+        else{
+            final =  Math.trunc(cents/quarter)
+                if(final % dime == 0){
+                    final = final + (final/dime)
+                    return final;
+                }
+                else{
+                    final = final + Math.trunc(final/dime)
+                    return final;
+                }
+            }
+        }
+    }
+    return final;
+}
+
+var answer3 = generatecoinchange(35);
+console.log(answer3);
